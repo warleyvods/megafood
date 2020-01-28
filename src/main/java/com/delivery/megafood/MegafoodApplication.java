4501144044c938486b0a65d82594c4e4e0dde04c
Warley Vinicus <warleyvods@gmail.com>
2020-01-28 15:37:09 -0300

    Initial commit

---
+++ src/main/java/com/delivery/megafood/MegafoodApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.megafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegafoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegafoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/delivery/megafood/MegafoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.megafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegafoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
